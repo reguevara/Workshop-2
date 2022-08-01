d16d4bc7650db8a86cf1b303684497cde8c9a471
rebegul <109938265+reguevara@users.noreply.github.com>
2022-08-01 15:17:00 +1000

    Other two walls

--- Assets/GenerateCube.cs
+++ Assets/GenerateCube.cs
@@ -71,9 +71,29 @@ public class GenerateCube : MonoBehaviour
             
             new Vector3(1.0f, -1.0f, -1.0f),
             new Vector3(1.0f, 1.0f, -1.0f),
-            new Vector3(1.0f, 1.0f, 1.0f)
+            new Vector3(1.0f, 1.0f, 1.0f),
 
             // Define more vertices here!
+
+            new Vector3(-1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, -1.0f, 1.0f),
+            new Vector3(1.0f, -1.0f, 1.0f),
+
+            new Vector3(1.0f, -1.0f, 1.0f),
+            new Vector3(1.0f, 1.0f, 1.0f),
+            new Vector3(-1.0f, 1.0f, 1.0f),
+
+            // Define more vertices here!
+
+            new Vector3(-1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, -1.0f, -1.0f),
+            new Vector3(-1.0f, -1.0f, -1.0f),
+            
+
+            new Vector3(1.0f, -1.0f, -1.0f),
+            new Vector3(-1.0f, 1.0f, -1.0f),
+            new Vector3(1.0f, 1.0f, -1.0f)
+            
         });
 
         // Step 2: Define the vertex colours. There is a one-to-one index
@@ -115,9 +135,27 @@ public class GenerateCube : MonoBehaviour
             
             Color.yellow,
             Color.yellow,
-            Color.yellow
+            Color.yellow,
             
             // Define more colours here!
+
+            Color.blue,
+            Color.blue,
+            Color.blue,
+
+            Color.blue,
+            Color.blue,
+            Color.blue,
+
+            // Define more colours here!
+
+            Color.blue,
+            Color.blue,
+            Color.blue,
+
+            Color.blue,
+            Color.blue,
+            Color.blue
         });
 
         // Step 3: Define the indices. The indices "connect" vertices together
